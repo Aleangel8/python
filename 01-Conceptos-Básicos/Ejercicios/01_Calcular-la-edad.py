67138bb3b16d368f61287a81afe0cd574d2327e2
Angel Alejandro Ruiz Centeno <aleangelrc@gmail.com>
2023-03-02 23:29:04 +0100

    Se implementa validaciones de entrada ejer 1

--- 01-Conceptos-Básicos/Ejercicios/01_Calcular-la-edad.py
+++ 01-Conceptos-Básicos/Ejercicios/01_Calcular-la-edad.py
@@ -7,26 +7,49 @@
 #  Si no es mayor mostrar cuanto le falta en años
 #   
 #      
-#       Falta validar datos
+#       
 #########################################################################################
 from datetime import datetime
 import time
+
 volver=1
 while(volver==1):
-
-    Fecha_Nacim=input("Escribe tu fecha de Nacimiento: ")
-    dt_actual = datetime.strptime(Fecha_Nacim, "%d-%m-%Y").date()
-
+#  Coge la fecha de nacimiento dt_bird y la valida
+    while(volver==1):
+        try:
+            Fecha_Nacim=input("Escribe tu fecha de Nacimiento: ")
+            dt_bird = datetime.strptime(Fecha_Nacim, "%d-%m-%Y").date()
+        except ValueError:
+            print("Fecha incorrecta")
+            volver=1
+        else:
+            volver=0
+#   Coge la fecha local today
     t1 = time.time()
     t_local = time.localtime(t1)
-    edad = t_local.tm_year-dt_actual.year
-
-    
+#   Calcula edad precisa
+    edad = t_local.tm_year-dt_bird.year
+    if(dt_bird.month>t_local.tm_mon):
+        edad -=1
+#   Print resultado
     print(f"La edad del usuario es: {edad}")
     if(edad<18):
         print(f"Es menor de edad, le faltan: {18-edad} años para ser mayor")
     else:
         print("Es mayor de edad")
-    volver=int(input("Desea volver a intentar 1:Si  0:NO : "))
+#   Check volver a intentar y validar entrada
+    volver2=1
+    while(volver2==1):   
+        try:
+            volver=(input("Pulse 0 para Salir, Cualquier otro numero volver a intentar : "))
+            if(int(volver)!=0):
+                volver=1   
+        except ValueError:
+            print("No se introdujo un numero")
+            volver2=1
+        else:
+            volver2=0
+
+
 
 
