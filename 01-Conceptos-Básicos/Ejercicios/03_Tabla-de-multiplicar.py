32ae7fdf17dc803683be008b939a8eaf76b7f7cf
Angel Alejandro Ruiz Centeno <aleangelrc@gmail.com>
2023-03-03 00:44:20 +0100

    Se hace eje 3 con for y while

--- 01-Conceptos-Básicos/Ejercicios/03_Tabla-de-multiplicar.py
+++ 01-Conceptos-Básicos/Ejercicios/03_Tabla-de-multiplicar.py
@@ -9,6 +9,43 @@
 #      
 #       Falta con el for y validar datos
 #########################################################################################
+
+
+#######       FOR
+
+volver=1
+while(volver==1):
+    while(volver==1):
+        try:
+            Numero = int(input("Introduzca un numero: "))
+        except:
+            print("Datos Incorrectos")
+            volver=1
+        else:
+            volver=0
+
+    for i in range(0, 11, 1):
+        print(int(Numero)*i)
+        i +=1
+
+
+#   Check volver a intentar y validar entrada
+    volver2=1
+    while(volver2==1):   
+        try:
+            volver=(input("Pulse 0 para Salir, Cualquier otro numero volver a intentar : "))
+            if(int(volver)!=0):
+                volver=1   
+        except ValueError:
+            print("No se introdujo un numero")
+            volver2=1
+        else:
+            volver2=0
+
+
+
+"""
+#######   WHILE
 volver=1
 while(volver==1):
     while(volver==1):
@@ -38,3 +75,6 @@ while(volver==1):
             volver2=1
         else:
             volver2=0
+
+
+"""
